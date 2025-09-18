a52da20458953e9b59f34b9be0e4ab20895bf22e
Drake <dariocostarelli88@gmail.com>
2025-09-18 11:14:19 +0100

    Create post_list.html and add it to views

--- blog/models.py
+++ blog/models.py
@@ -31,7 +31,7 @@ class Comment(models.Model):
         Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter")
-    body = body = models.TextField()
+    body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
