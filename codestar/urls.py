dc86288c8468252ef3c27f3602fe274a11dd091b
Drake <dariocostarelli88@gmail.com>
2025-09-21 15:30:27 +0100

    Load sample posts via fixture (loaddata); ignore blog/fixtures in git

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls'), name='blog-urls'),
-    path('', RedirectView.as_view(url='/blog/', permanent=False)),
+    path("admin/", admin.site.urls),
+    path("blog/", include("blog.urls"), name="blog-urls"),
+    path('summernote/', include('django_summernote.urls')),
 ]
