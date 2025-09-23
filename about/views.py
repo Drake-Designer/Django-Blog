44bded380dcd1c70a33962409d045b6cd98c7592
Drake <dariocostarelli88@gmail.com>
2025-09-23 11:23:47 +0100

    Add About app with model, view, template, and navigation link

--- about/views.py
+++ about/views.py
@@ -4,6 +4,6 @@ from .models import About
 # Create your views here.
 
 
-def about_page(request):
-    record = About.objects.first()
+def about_me(request):
+    record = About.objects.order_by('-updated_on').first()
     return render(request, 'about/about.html', {'about': record})
--- codestar/urls.py
+++ codestar/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("about/", include("about.urls"), name="about-urls"),
     path("admin/", admin.site.urls),
-    path("blog/", include("blog.urls"), name="blog-urls"),
-    path('summernote/', include('django_summernote.urls')),
-    path("about/", include("about.urls")),
+    path("summernote/", include("django_summernote.urls")),
+    path("", include("blog.urls"), name="blog-urls"),
 ]
